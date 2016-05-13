c4dd1d30f076c874d7fb572ebcfc3bb6dd7124eb
jmeth <jmeth@users.noreply.github.com>
2016-05-12 22:42:27 -0600

    sshpass and vagrant patch now built by packer

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
+#Update system and install necessary packages for ansible
 yum -y update
 yum -y install epel-release
-yum -y install wget curl openssh-server vim git gcc libffi-devel openssl-devel python-devel python-pip
+yum -y install wget curl openssh-server vim git gcc libffi-devel openssl-devel python-devel python-pip sshpass
 pip install --upgrade pip
 pip install --upgrade ansible
+
+#Patch to fix Vagrant 1.8.1 not being able to find Ansible > 2.0
+## See https://github.com/mitchellh/vagrant/issues/6793
+echo "Patching ansible-galaxy"
+galaxy="/usr/bin/ansible-galaxy"
+patch='if sys.argv == ["/usr/bin/ansible-galaxy", "--help"]: sys.argv.insert(1, "info")'
+[[ ! -f ${galaxy} ]] || grep -F -q "${patch}" ${galaxy} || sudo sed -i "/__main__/a \\    ${patch}" ${galaxy}
